f4d56f2b741f2e5e9c317823dc818821008e2891
ximena815 <68305685+ximena815@users.noreply.github.com>
2021-01-28 17:11:44 -0500

    Add files via upload

---
+++ RetoTutoria1/RetoTutoria1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RetoTutoria1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Entrada de la variable X1
+            Console.WriteLine("Ingrese X1: ");
+            double x1 = double.Parse(Console.ReadLine());
+
+            // Entrada de la variable X2
+            Console.WriteLine("Ingrese X2: ");
+            double x2 = double.Parse(Console.ReadLine());
+
+            // Entrada de la variable Y1
+            Console.WriteLine("Ingrese Y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            // Entrada de la variable Y2
+            Console.WriteLine("Ingrese Y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            // Variable m
+            double m = (y2 - y1) / (x2 - x1); //Formula
+            Console.WriteLine("Ingrese m: " + m);
+
+            //Variable b
+            double b = y1 - (m * x1);
+            Console.WriteLine("Ingrese b: " + b);
+
+            //Variable b
+            double d = Math.Sqrt (((x2-x1)* (x2 - x1))+((y2 - y1) * (y2 - y1)));
+            Console.WriteLine("Ingrese d: " + d);
+
+        }
+    }
+}
